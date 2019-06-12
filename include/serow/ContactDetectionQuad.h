814d6007b195d3d76a3365e56495f09361bd0670
Stylianos Piperakis <stpiperakis@gmail.com>
2019-06-12 16:30:41 +0300

    added SM for contact detection

--- include/serow/ContactDetectionQuad.h
+++ include/serow/ContactDetectionQuad.h
@@ -1,8 +1,6 @@
 #include <serow/Gaussian.h>
 #include <string.h>
 
-
-
 namespace serow
 {
 class ContactDetectionQuad
@@ -15,13 +13,13 @@ private:
   double sigmaLFv, sigmaLHv, sigmaRFv, sigmaRHv;
   double xmin, xmax, ymin, ymax;
   double LFfmin, LHfmin, RFfmin, RHfmin;
-  double pLFf,pLHf, pRFf, pRHf, pLFc, pLHc, pRFc, pRHc, pLFv, pLHv, pRFv, pRHv;
-  double pLF, pLH, pRF, pRH, p;
+  double pLFf, pLHf, pRFf, pRHf, pLFc, pLHc, pRFc, pRHc, pLFv, pLHv, pRFv, pRHv;
+  double pLF, pLH, pRF, pRH, p, sum;
   double VelocityThres;
   bool firstContact, useCOP, useKin;
   int contactLF, contactLH, contactRF, contactRH;
   double prob_TH;
-  std::string support_foot_frame, support_leg, LFfoot_frame, LHfoot_frame, RFfoot_frame, RHfoot_frame;
+  std::string support_foot_frame, support_leg, LFfoot_frame, LHfoot_frame, RFfoot_frame, RHfoot_frame, phase;
 
   double deltaLFfz, deltaLHfz, deltaRFfz, deltaRHfz, LFf_, LHf_, RFf_, RHf_;
   //Smidtt Trigger detection
@@ -37,7 +35,6 @@ private:
     return gs.cdf(vel_min, v, sigma);
   }
 
-
   double computeCOPContactProb(double max, double min, double sigma, double cop)
   {
     if (cop != 0)
@@ -50,11 +47,11 @@ private:
     }
   }
 
-  double computeContactProb(double LFf,double LHf,double RFf,double RHf,
-                            double copLFx,double copLFy,double  copLHx,
-                            double copLHy,double copRFx,double copRFy,
-                            double copRHx,double copRHy,double LFvnorm,
-                            double LHvnorm,double RFvnorm,double RHvnorm)
+  double computeContactProb(double LFf, double LHf, double RFf, double RHf,
+                            double copLFx, double copLFy, double copLHx,
+                            double copLHy, double copRFx, double copRFy,
+                            double copRHx, double copRHy, double LFvnorm,
+                            double LHvnorm, double RFvnorm, double RHvnorm)
   {
 
     pLFf = computeForceContactProb(LFfmin, sigmaLFf, LFf);
@@ -69,12 +66,7 @@ private:
     pRHf = computeForceContactProb(RHfmin, sigmaRHf, RHf);
     pRH = pRHf;
 
-    cout<<"PROB LF RF LH RH"<<endl;
-    cout<<pLF<<endl;
-    cout<<pRF<<endl;
-    cout<<pLH<<endl;
-    cout<<pRH<<endl;
-
+ 
 
     //To utilize the full contact-wrench - careful for non-coplanar contacts
     if (useCOP)
@@ -106,30 +98,28 @@ private:
       pRH *= pRHv;
     }
 
+    p = pLF + pLH + pRF + pRH;
 
-
-    // p = pLF + pLH + pRF + pRH;
-
-    // if (p != 0)
-    // {
-    //   pLF = pLF / p;
-    //   pRF = pRF / p;
-    //   pLH = pLH / p;
-    //   pRH = pRH / p;
-    //   pLF += 0.75;
-    //   pRF += 0.75;
-    //   pLH += 0.75;
-    //   pRH += 0.75;
-    // }
-    // else
-    // {
-    //   pLF = 0;
-    //   pRF = 0;
-    //   pLH = 0;
-    //   pRH = 0;
-    // }
-
-
+    if (p != 0)
+    {
+      pLF = pLF / p;
+      pRF = pRF / p;
+      pLH = pLH / p;
+      pRH = pRH / p;
+    }
+    else
+    {
+      pLF = 0;
+      pRF = 0;
+      pLH = 0;
+      pRH = 0;
+    }
+    cout << "PROB LF RF LH RH" << endl;
+    cout << pLF << endl;
+    cout << pRF << endl;
+    cout << pLH << endl;
+    cout << pRH << endl;
+    
     contactLH = 0;
     if (pLF >= prob_TH)
       contactLF = 1;
@@ -137,20 +127,19 @@ private:
     contactRF = 0;
     if (pRF >= prob_TH)
       contactRF = 1;
-    
-    
+
     contactLH = 0;
     if (pLH >= prob_TH)
       contactLH = 1;
 
     contactRH = 0;
     if (pRH >= prob_TH)
-      contactRH = 1;  
+      contactRH = 1;
   }
 
 public:
-  void init(std::string LFfoot_frame_, std::string  LHfoot_frame_, std::string RFfoot_frame_, std::string  RHfoot_frame_, double LFfmin_, double LHfmin_, double RFfmin_, double RHfmin_, double xmin_, double xmax_,
-            double ymin_, double ymax_, double sigmaLFf_, double sigmaLHf_, double sigmaRFf_, double sigmaRHf_, double sigmaLFc_, double sigmaLHc_, double sigmaRFc_,  double sigmaRHc_, double VelocityThres_, 
+  void init(std::string LFfoot_frame_, std::string LHfoot_frame_, std::string RFfoot_frame_, std::string RHfoot_frame_, double LFfmin_, double LHfmin_, double RFfmin_, double RHfmin_, double xmin_, double xmax_,
+            double ymin_, double ymax_, double sigmaLFf_, double sigmaLHf_, double sigmaRFf_, double sigmaRHf_, double sigmaLFc_, double sigmaLHc_, double sigmaRFc_, double sigmaRHc_, double VelocityThres_,
             double sigmaLFv_, double sigmaLHv_, double sigmaRFv_, double sigmaRHv_, bool useCOP_ = false, bool useKin_ = false, double prob_TH_ = 0.9)
   {
     LFfoot_frame = LFfoot_frame_;
@@ -173,7 +162,6 @@ public:
     sigmaLHf = sigmaLHf_;
     sigmaRFf = sigmaRFf_;
     sigmaRHf = sigmaRHf_;
-
 
     sigmaLFc = sigmaLFc_;
     sigmaLHc = sigmaLHc_;
@@ -198,7 +186,6 @@ public:
     pRF = 0;
     pRH = 0;
 
-
     pLFf = 0;
     pLHf = 0;
 
@@ -210,7 +197,6 @@ public:
 
     pRFc = 0;
     pRHc = 0;
-
 
     pLFv = 0;
     pLHv = 0;
@@ -229,7 +215,6 @@ public:
 
     deltaRFfz = 0;
     deltaRHfz = 0;
-
   }
 
   void init(std::string LFfoot_frame_, std::string LHfoot_frame_, std::string RFfoot_frame_, std::string RHfoot_frame_, double LegHighThres_, double LegLowThres_, double StrikingContact_, double VelocityThres_, double prob_TH_ = 0.9)
@@ -246,7 +231,7 @@ public:
 
     StrikingContact = StrikingContact_;
     VelocityThres = VelocityThres_;
-    
+
     firstContact = true;
     prob_TH = prob_TH_;
     pLF = 0;
@@ -266,90 +251,79 @@ public:
 
     deltaRFfz = 0;
     deltaRHfz = 0;
-
   }
   double getLFDiffForce()
   {
-     return fabs(deltaLFfz);
+    return fabs(deltaLFfz);
   }
 
   double getRFDiffForce()
   {
-     return fabs(deltaRFfz);
+    return fabs(deltaRFfz);
   }
   double getLHDiffForce()
   {
-     return fabs(deltaLHfz);
+    return fabs(deltaLHfz);
   }
   double getRHDiffForce()
   {
-     return fabs(deltaRHfz);
+    return fabs(deltaRHfz);
   }
-  void computeSupportFoot(double LFf, double LHf, double RFf, double RHf, 
+  void computeSupportFoot(double LFf, double LHf, double RFf, double RHf,
                           double copLFx, double copLFy, double copLHx, double copLHy,
                           double copRFx, double copRFy, double copRHx, double copRHy,
                           double LFvnorm, double LHvnorm, double RFvnorm, double RHvnorm)
   {
 
-   
-    computeContactProb(LFf, LHf, RFf, RHf, copLFx, copLFy,  copLHx, copLHy, copRFx, copRFy, copRHx, copRHy, LFvnorm, LHvnorm, RFvnorm, RHvnorm);
-    //Choose Initial Support Foot based on Contact Force
-    // if (firstContact)
-    // {
-      if (pLF >= pLH && pLF >= pRF && pLF >= pRH)
-      {
-        // Initial support leg
-        support_leg = "LFLeg";
-        support_foot_frame = LFfoot_frame;
-      }
-      else if(pLH >= pLF && pLH >= pRF && pLH >= pRH)
-      {
-        support_leg = "LHLeg";
-        support_foot_frame = LHfoot_frame;
-      }
-      else if(pRF >= pLF && pRF >= pLH && pRF >= pRH)
-      {
-        support_leg = "RFLeg";
-        support_foot_frame = RFfoot_frame;
-      }
-      else
-      {
-        support_leg = "RHLeg";
-        support_foot_frame = RHfoot_frame;
-      }
-    // }
-    // else
-    // {
-    //   if ((pLF >= pLH && pLF >= pRF && pLF >= pRH) && (LFvnorm < VelocityThres && LHvnorm > VelocityThres && RFvnorm > VelocityThres && RHvnorm > VelocityThres))
-    //   {
-    //     support_leg = "LFLeg";
-    //     support_foot_frame = LFfoot_frame;
-    //   }
-    //   else if ((pLH >= pLF && pLH >= pRF && pLH >= pRH) && (LHvnorm < VelocityThres && LFvnorm > VelocityThres && RFvnorm > VelocityThres && RHvnorm > VelocityThres))
-    //   {
-    //     support_leg = "LHLeg";
-    //     support_foot_frame = LHfoot_frame;
-    //   }
-    //   else if (pRF >= pLF && pRF >= pLH && pRF >= pRH && (RFvnorm < VelocityThres && LFvnorm > VelocityThres && LHvnorm > VelocityThres && RHvnorm > VelocityThres))
-    //   {
-    //     support_leg = "RFLeg";
-    //     support_foot_frame = RFfoot_frame;
-    //   }
-    //   else if(pRH >= pLF && pRH >= pLH && pRH >= pRF && (RHvnorm < VelocityThres && LFvnorm > VelocityThres && LHvnorm > VelocityThres && RFvnorm > VelocityThres))
-    //   {
-    //     support_leg = "RHLeg";
-    //     support_foot_frame = RHfoot_frame;
-    //   }
-    // }
+    computeContactProb(LFf, LHf, RFf, RHf, copLFx, copLFy, copLHx, copLHy, copRFx, copRFy, copRHx, copRHy, LFvnorm, LHvnorm, RFvnorm, RHvnorm);
+    if (pLF > pLH && pLF > pRF && pLF > pRH)
+    {
+      // Initial support leg
+      support_leg = "LFLeg";
+      support_foot_frame = LFfoot_frame;
+    }
+    else if (pLH > pLF && pLH > pRF && pLH > pRH)
+    {
+      support_leg = "LHLeg";
+      support_foot_frame = LHfoot_frame;
+    }
+    else if (pRF > pLF && pRF > pLH && pRF > pRH)
+    {
+      support_leg = "RFLeg";
+      support_foot_frame = RFfoot_frame;
+    }
+    else
+    {
+      support_leg = "RHLeg";
+      support_foot_frame = RHfoot_frame;
+    }
 
-    if(firstContact)
+    sum = contactRH + contactRF + contactLF + contactRH;
+
+    switch (sum)
+    {
+    case 0:
+      phase = "None";
+      break;
+    case 1:
+      phase = "Single Support";
+      break;
+    case 2:
+      phase = "Double Support";
+      break;
+    case 3:
+      phase = "Triple Support";
+    case 4:
+      phase = "Quad Support";
+    }
+
+    if (firstContact)
     {
       deltaLFfz = 0;
       deltaRFfz = 0;
       deltaLHfz = 0;
       deltaRHfz = 0;
       firstContact = false;
-
     }
     else
     {
@@ -358,12 +332,11 @@ public:
       deltaLHfz = LHf - LHf_;
       deltaRHfz = RHf - RHf_;
     }
-    
+
     LFf_ = LFf;
     RFf_ = RFf;
     LHf_ = LHf;
     RHf_ = RHf;
-
   }
 
   double getLFLegContactProb()
@@ -406,6 +379,11 @@ public:
   {
     return support_foot_frame;
   }
+
+  std::string getSupportPhase()
+  {
+    return phase;
+  }
   std::string getSupportLeg()
   {
     return support_leg;
@@ -413,70 +391,31 @@ public:
 
   void SchmittTriggerWithKinematics(double LFf, double LHf, double RFf, double RHf, double LFvnorm, double LHvnorm, double RFvnorm, double RHvnorm)
   {
-    contactLF = 0;
-    contactLH = 0;
 
-    contactRF = 0;
-    contactRH = 0;
-
-    p = LFf + LFf + RFf + RHf;
-    pLF = 0;
-    pRF = 0;
-    pLH = 0;
-    pRH = 0;
-
-    if (p != 0)
+    if (LFf > LHf && LFf > RFf && LFf > RHf)
     {
-      pLF = LFf / p + 0.5;
-      pRF = RFf / p + 0.5;
-      pLH = LHf / p + 0.5;
-      pRH = RHf / p + 0.5;
+      support_leg = "LFLeg";
+      support_foot_frame = LFfoot_frame;
     }
-
-    if (firstContact)
+    else if (LHf > LFf && LHf > RFf && LHf > RHf)
     {
-      if (LFf >= LHf && LFf >= RFf && LFf >= RHf)
-      {
-        // Initial support leg
-        support_leg = "LFLeg";
-        support_foot_frame = LFfoot_frame;
-      }
-      else if (LHf >= LFf && LHf >= RFf && LHf >= RHf)
-      {
-        support_leg = "LHLeg";
-        support_foot_frame = LHfoot_frame;
-      }
-      else if (RFf >= LFf && RFf >= LHf && RFf >= RHf)
-      {
-        support_leg = "RFLeg";
-        support_foot_frame = RFfoot_frame;
-      }
-      else 
-      {
-        support_leg = "RHLeg";
-        support_foot_frame = RHfoot_frame;
-      }
-
-      if (pLF >= prob_TH)
-        contactLF = 1;
-
-      contactRF = 0;
-      if (pRF >= prob_TH)
-        contactRF = 1;
-
-      if (pLH >= prob_TH)
-        contactLH = 1;
-
-      contactRH = 0;
-      if (pRH >= prob_TH)
-        contactRH = 1;
-
-      firstContact = false;
+      support_leg = "LHLeg";
+      support_foot_frame = LHfoot_frame;
+    }
+    else if (RFf > LFf && RFf > LHf && RFf > RHf)
+    {
+      support_leg = "RFLeg";
+      support_foot_frame = RFfoot_frame;
     }
     else
     {
-      //Check if Left Leg is support
-      if (LFvnorm < VelocityThres)
+      support_leg = "RHLeg";
+      support_foot_frame = RHfoot_frame;
+    }
+
+    if (LFvnorm <= VelocityThres)
+    {
+      if (contaLF == 0)
       {
         if (LFf > LegHighThres && LFf < StrikingContact)
         {
@@ -490,8 +429,11 @@ public:
           contactLF = 0;
         }
       }
+    }
 
-      if (LHvnorm < VelocityThres)
+    if (LHvnorm <= VelocityThres)
+    {
+      if (contactLH == 0)
       {
         if (LHf > LegHighThres && LHf < StrikingContact)
         {
@@ -505,8 +447,11 @@ public:
           contactLH = 0;
         }
       }
+    }
 
-      if (RFvnorm < VelocityThres)
+    if (RFvnorm <= VelocityThres)
+    {
+      if (contactRF == 0)
       {
         if (RFf > LegHighThres && RFf < StrikingContact)
         {
@@ -520,7 +465,11 @@ public:
           contactRF = 0;
         }
       }
-      if (RHvnorm < VelocityThres)
+    }
+
+    if (RHvnorm <= VelocityThres)
+    {
+      if (contactRH == 0)
       {
         if (RHf > LegHighThres && RHf < StrikingContact)
         {
@@ -534,151 +483,192 @@ public:
           contactRH = 0;
         }
       }
-
-
-
-      //Determine support
-      //Quad Support
-      if (contactLF && contactRF && contactLH && contactRH)
-      {
-        if (LFf >= LHf && LFf >= RFf && LFf >=RHf)
-        {
-          support_leg = "LFLeg";
-          support_foot_frame = LFfoot_frame;
-        }
-        else if (LHf >= LFf && LHf >= RFf && LHf >= RHf)
-        {
-          support_leg = "LHLeg";
-          support_foot_frame = LHfoot_frame;
-        }
-        else if (RFf >= LFf && RFf >= LHf && RFf >= RHf)
-        {
-          support_leg = "RFLeg";
-          support_foot_frame = RFfoot_frame;
-        }
-        else 
-        {
-          support_leg = "RHLeg";
-          support_foot_frame = RHfoot_frame;
-        }
-      }
-      //Triple Support
-      //LF-RH-RF --- no LH
-      else if(contactLF && contactRH && contactRF)
-      {
-        if (LFf >= RFf && LFf >=RHf)
-        {
-          support_leg = "LFLeg";
-          support_foot_frame = LFfoot_frame;
-        }
-        else if (RFf >= LFf && RFf >= RHf)
-        {
-          support_leg = "RFLeg";
-          support_foot_frame = RFfoot_frame;
-        }
-        else 
-        {
-          support_leg = "RHLeg";
-          support_foot_frame = RHfoot_frame;
-        }
-      }
-      //LF-LH-RF ---- no RH
-      else if(contactLF && contactLH && contactRF)
-      {
-        if (LFf >= RFf && LFf >=LHf)
-        {
-          support_leg = "LFLeg";
-          support_foot_frame = LFfoot_frame;
-        }
-        else if (RFf >= LFf && RFf >= LHf)
-        {
-          support_leg = "RFLeg";
-          support_foot_frame = RFfoot_frame;
-        }
-        else 
-        {
-          support_leg = "RHLeg";
-          support_foot_frame = RHfoot_frame;
-        }
-      }
-      //LF-RH-LH -- RF
-      else if(contactLF && contactLH && contactRH)
-      {
-        if (LFf >= RHf && LFf >=LHf)
-        {
-          support_leg = "LFLeg";
-          support_foot_frame = LFfoot_frame;
-        }
-        else if (RHf >= LFf && RHf >= LHf)
-        {
-          support_leg = "RHLeg";
-          support_foot_frame = RHfoot_frame;
-        }
-        else 
-        {
-          support_leg = "RFLeg";
-          support_foot_frame = RFfoot_frame;
-        }
-      }
-      //RF-RH-LH -- no LF
-      else if(contactRF && contactLH && contactRH)
-      {
-        if (RFf >= RHf && RFf >=LHf)
-        {
-          support_leg = "RFLeg";
-          support_foot_frame = RFfoot_frame;
-        }
-        else if (RHf >= RFf && RHf >= LHf)
-        {
-          support_leg = "RHLeg";
-          support_foot_frame = RHfoot_frame;
-        }
-        else 
-        {
-          support_leg = "LHLeg";
-          support_foot_frame = LHfoot_frame;
-        }
-      }
-      //Double Support
-      //TO DO
-
-      //Single Support
-      else if (contactLF)
-      {
-        support_leg = "LFLeg";
-        support_foot_frame = LFfoot_frame;
-      }
-      else if (contactLH)
-      {
-        support_leg = "LHLeg";
-        support_foot_frame = LHfoot_frame;
-      }
-      else if (contactRF)
-      {
-        support_leg = "RFLeg";
-        support_foot_frame = RFfoot_frame;
-      }
-      else if (contactRH)
-      {
-        support_leg = "RHLeg";
-        support_foot_frame = RHfoot_frame;
-      }
     }
 
+    sum = contactRH + contactRF + contactLF + contactRH;
 
+    switch (sum)
+    {
+    case 0:
+      phase = "None";
+      break;
+    case 1:
+      phase = "Single Support";
+      break;
+    case 2:
+      phase = "Double Support";
+      break;
+    case 3:
+      phase = "Triple Support";
+    case 4:
+      phase = "Quad Support";
+    }
 
-
-    if(!firstContact)
+    if (!firstContact)
     {
       deltaLFfz = LFf - LFf_;
       deltaRFfz = RFf - RFf_;
       deltaLHfz = LHf - LHf_;
       deltaRHfz = RHf - RHf_;
     }
+    else
+    {
+      deltaLFfz = 0;
+      deltaRFfz = 0;
+      deltaLHfz = 0;
+      deltaRHfz = 0;
+      firstContact = false;
+    }
+
+    LFf_ = LFf;
+    RFf_ = RFf;
+    LHf_ = LHf;
+    RHf_ = RHf;
+  }
+  void computeForceWeights(double LFf, double LHf, double RFf, double RHf)
+  {
+    p = LFf + LFf + RFf + RHf;
+    pLF = 0;
+    pRF = 0;
+    pLH = 0;
+    pRH = 0;
+
+    if (p != 0)
+    {
+      pLF = LFf / p;
+      pRF = RFf / p;
+      pLH = LHf / p;
+      pRH = RHf / p;
+    }
+  }
+  void SchmittTrigger(double LFf, double LHf, double RFf, double RHf)
+  {
+    contactLF = 0;
+    contactLH = 0;
+
+    contactRF = 0;
+    contactRH = 0;
+
+    if (LFf >= LHf && LFf >= RFf && LFf >= RHf)
+    {
+      support_leg = "LFLeg";
+      support_foot_frame = LFfoot_frame;
+    }
+    else if (LHf >= LFf && LHf >= RFf && LHf >= RHf)
+    {
+      support_leg = "LHLeg";
+      support_foot_frame = LHfoot_frame;
+    }
+    else if (RFf >= LFf && RFf >= LHf && RFf >= RHf)
+    {
+      support_leg = "RFLeg";
+      support_foot_frame = RFfoot_frame;
+    }
+    else
+    {
+      support_leg = "RHLeg";
+      support_foot_frame = RHfoot_frame;
+    }
+
+    if (contaLF == 0)
+    {
+      if (LFf > LegHighThres && LFf < StrikingContact)
+      {
+        contactLF = 1;
+      }
+    }
+    else
+    {
+      if (LFf < LegLowThres)
+      {
+        contactLF = 0;
+      }
+    }
+
+    if (contactLH == 0)
+    {
+      if (LHf > LegHighThres && LHf < StrikingContact)
+      {
+        contactLH = 1;
+      }
+    }
+    else
+    {
+      if (LHf < LegLowThres)
+      {
+        contactLH = 0;
+      }
+    }
+
+    if (contactRF == 0)
+    {
+      if (RFf > LegHighThres && RFf < StrikingContact)
+      {
+        contactRF = 1;
+      }
+    }
+    else
+    {
+      if (RFf < LegLowThres)
+      {
+        contactRF = 0;
+      }
+    }
+
+    if (contactRH == 0)
+    {
+      if (RHf > LegHighThres && RHf < StrikingContact)
+      {
+        contactRH = 1;
+      }
+    }
+    else
+    {
+      if (RHf < LegLowThres)
+      {
+        contactRH = 0;
+      }
+    }
+
+    sum = contactRH + contactRF + contactLF + contactRH;
+
+    switch (sum)
+    {
+    case 0:
+      phase = "None";
+      break;
+    case 1:
+      phase = "Single Support";
+      break;
+    case 2:
+      phase = "Double Support";
+      break;
+    case 3:
+      phase = "Triple Support";
+    case 4:
+      phase = "Quad Support";
+    }
+
+    if (!firstContact)
+    {
+      deltaLFfz = LFf - LFf_;
+      deltaRFfz = RFf - RFf_;
+      deltaLHfz = LHf - LHf_;
+      deltaRHfz = RHf - RHf_;
+    }
+    else
+    {
+      deltaLFfz = 0;
+      deltaRFfz = 0;
+      deltaLHfz = 0;
+      deltaRHfz = 0;
+      firstContact = false;
+    }
+
     LFf_ = LFf;
     RFf_ = RFf;
     LHf_ = LHf;
     RHf_ = RHf;
   }
 };
-}
+} // namespace serow
