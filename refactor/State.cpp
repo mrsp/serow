782314fe51688978ec1bc4d44c951eec30115a1e
mrsp <stpiperakis@gmail.com>
2023-08-25 13:16:27 +0300

    refactored State

--- refactor/State.cpp
+++ refactor/State.cpp
@@ -4,62 +4,137 @@
  */
 #include "State.hpp"
 
-Eigen::Isometry3d State::getBasePose() const { return base_pose_; }
+State::State(std::unordered_set<std::string> contacts_frame, bool point_feet) {
+    contacts_frame_ = std::move(contacts_frame);
+    num_leg_ee_ = contacts_frame_.size();
+    point_feet_ = point_feet;
+}
+const Eigen::Isometry3d& State::getBasePose() const {
+    Eigen::Isometry3d base_pose = Eigen::Isometry3d::Identity();
+    base_pose.linear() = base_orientation_.toRotationMatrix();
+    base_pose.translation() = base_position_;
+    return base_pose;
+}
 
-Eigen::Vector3d State::getBasePosition() const { return base_position_; }
+const Eigen::Vector3d& State::getBasePosition() const { return base_position_; }
 
-Eigen::Quaterniond State::getBaseOrientation() const { return base_orientation_; }
+const Eigen::Quaterniond& State::getBaseOrientation() const { return base_orientation_; }
 
-Eigen::Vector3d State::getBaseLinearVelocity() const { return base_linear_velocity_; }
+const Eigen::Vector3d& State::getBaseLinearVelocity() const { return base_linear_velocity_; }
 
-Eigen::Vector3d State::getBaseAngularVelocity() const { return base_angular_velocity_; }
+const Eigen::Vector3d& State::getBaseAngularVelocity() const { return base_angular_velocity_; }
 
-Eigen::Vector3d State::getImuLinearAccelarationBias() const {
+const Eigen::Vector3d& State::getImuLinearAccelarationBias() const {
     return imu_linear_acceleration_bias_;
 }
 
-Eigen::Vector3d State::getImuAngularVelocityBias() const { return imu_angular_velocity_bias_; }
+const Eigen::Vector3d& State::getImuAngularVelocityBias() const {
+    return imu_angular_velocity_bias_;
+}
 
-std::optional<Eigen::Isometry3d> State::getFootPose(const std::string &frame_name) const {
-    if (foot_pose_.count(frame_name))
-        return foot_pose_.at(frame_name);
+std::optional<Eigen::Vector3d> State::getContactPosition(const std::string& frame_name) const {
+    // If the end-effector is in contact with the environment and we have a contact position 
+    // available
+    if (contacts_status_.count(frame_name) && contacts_status_.at(frame_name) &&
+        contacts_position_.count(frame_name))
+        return contacts_position_.at(frame_name);
     else
         return std::nullopt;
 }
 
-std::unordered_set<std::string> State::getFootFrames() const { return foot_frames_; }
-
-std::optional<bool> State::getFootContactStatus(const std::string &frame_name) const {
-    if (foot_contact_.count(frame_name))
-        return foot_contact_.at(frame_name);
+std::optional<Eigen::Quaterniond> State::getContactOrientation(
+    const std::string& frame_name) const {
+    // If the end-effector is in contact with the environment and we have a contact orientation
+    // available
+    if (contacts_status_.count(frame_name) && contacts_status_.at(frame_name) &&
+        contacts_orientation_.has_value() && contacts_orientation_.value().count(frame_name))
+        return contacts_orientation_.value().at(frame_name);
     else
         return std::nullopt;
 }
 
-Eigen::Matrix<double, 6, 6> State::getBasePoseCov() const { return base_pose_cov_; }
+std::optional<Eigen::Isometry3d> State::getContactPose(const std::string &frame_name) const {
+    // If the end-effector is in contact with the environment and we have a contact orientation
+    // available
+    if (contacts_status_.count(frame_name) && contacts_status_.at(frame_name) &&
+        contacts_position_.count(frame_name) && contacts_orientation_.has_value() &&
+        contacts_orientation_.value().count(frame_name)) {
+        Eigen::Isometry3d contact_pose = Eigen::Isometry3d::Identity();
+        contact_pose.linear() = contacts_orientation_.value().at(frame_name).toRotationMatrix();
+        contact_pose.translation() = contacts_position_.at(frame_name);
+        return contact_pose;
+    } else {
+        return std::nullopt;
+    }
+}
 
-Eigen::Matrix3d State::getBasePositionCov() const { return base_position_cov_; }
+const std::unordered_set<std::string>& State::getContactsFrame() const { return contacts_frame_; }
 
-Eigen::Matrix3d State::getBaseOrientationCov() const { return base_orientation_cov_; }
+std::optional<bool> State::getContactStatus(const std::string &frame_name) const {
+    if (contacts_status_.count(frame_name))
+        return contacts_status_.at(frame_name);
+    else
+        return std::nullopt;
+}
 
-Eigen::Matrix3d State::getBaseLinearVelocityCov() const { return base_linear_velocity_cov_; }
+const Eigen::Matrix<double, 6, 6>& State::getBasePoseCov() const {
+    Eigen::Matrix<double, 6, 6> base_pose_cov = Eigen::Matrix<double, 6, 6>::Identity();
+    base_pose_cov.block<3, 3>(0, 0) = base_position_cov_;
+    base_pose_cov.block<3, 3>(3, 3) = base_orientation_cov_;
+    return base_pose_cov;
+}
 
-Eigen::Matrix3d State::getBaseAngularVelocityCov() const { return base_angular_velocity_cov_; }
+const Eigen::Matrix3d& State::getBasePositionCov() const { return base_position_cov_; }
 
-Eigen::Matrix3d State::getImuLinearAccelerationBiasCov() const {
+const Eigen::Matrix3d& State::getBaseOrientationCov() const { return base_orientation_cov_; }
+
+const Eigen::Matrix3d& State::getBaseLinearVelocityCov() const { return base_linear_velocity_cov_; }
+
+const Eigen::Matrix3d& State::getBaseAngularVelocityCov() const { return base_angular_velocity_cov_; }
+
+const Eigen::Matrix3d& State::getImuLinearAccelerationBiasCov() const {
     return imu_linear_acceleration_bias_cov_;
 }
 
-Eigen::Matrix3d State::getImuAngularVelocityBiasCov() const {
+const Eigen::Matrix3d& State::getImuAngularVelocityBiasCov() const {
     return imu_angular_velocity_bias_cov_;
 }
 
-std::optional<Eigen::Matrix<double, 6, 6>> State::getFootPoseCov(
-    const std::string &frame_name) const {
-    if (foot_pose_cov_.count(frame_name))
-        return foot_pose_cov_.at(frame_name);
+std::optional<Eigen::Matrix3d> State::getContactPositionCov(const std::string& frame_name) const {
+    // If the end-effector is in contact with the environment and we have a contact position
+    // covariance available
+    if (contacts_status_.count(frame_name) && contacts_status_.at(frame_name) &&
+        contacts_position_cov_.count(frame_name))
+        return contacts_position_cov_.at(frame_name);
     else
         return std::nullopt;
+}
+
+std::optional<Eigen::Matrix3d> State::getContactOrientationCov(
+    const std::string& frame_name) const {
+    // If the end-effector is in contact with the environment and we have a contact orientation
+    // covariance available
+    if (contacts_status_.count(frame_name) && contacts_status_.at(frame_name) &&
+        contacts_orientation_cov_.has_value() && contacts_orientation_.value().count(frame_name))
+        return contacts_orientation_cov_.value().at(frame_name);
+    else
+        return std::nullopt;
+}
+
+std::optional<Eigen::Matrix<double, 6, 6>> State::getContactPoseCov(
+    const std::string& frame_name) const {
+    // If the end-effector is in contact with the environment and we have a contact pose
+    // covariance available
+    if (contacts_status_.count(frame_name) && contacts_status_.at(frame_name) &&
+        contacts_position_cov_.count(frame_name) && contacts_orientation_cov_.has_value() &&
+        contacts_orientation_cov_.value().count(frame_name)) {
+        Eigen::Matrix<double, 6, 6> contact_pose_cov = Eigen::Matrix<double, 6, 6>::Identity();
+        contact_pose_cov.block<3, 3>(0, 0) = contacts_position_cov_.at(frame_name);
+        contact_pose_cov.block<3, 3>(3, 3) = contacts_orientation_cov_.value().at(frame_name);
+        return contact_pose_cov;
+    } else {
+        return std::nullopt;
+    }
 }
 
 void update(State state) {
--- refactor/test.cpp
+++ refactor/test.cpp
@@ -5,14 +5,12 @@
 #include "State.hpp"
 
 int main() {
-    State state;
-    state.point_feet_ =  true;
-    state.num_leg_ee_ = 1; 
-    Eigen::Isometry3d foot_pose = Eigen::Isometry3d::Identity();
-    foot_pose.translation() = Eigen::Vector3d(0.1, 0.5, -0.5);
-    state.foot_pose_.insert({"left_foot", foot_pose});
-    state.foot_frames_.insert({"left_foot"});
-    state.foot_contact_.insert({"left_foot", true});
+    std::unordered_set<std::string> contacts_frame;
+    contacts_frame.insert({"left_foot"});
+    State state(contacts_frame, true);
+
+    state.contacts_position_.insert({"left_foot", Eigen::Vector3d(0.1, 0.5, -0.5)});
+    state.contacts_status_.insert({"left_foot", true});
 
     ContactEKF base_ekf;
     Eigen::Vector3d g = Eigen::Vector3d(0, 0, -9.81);
@@ -36,6 +34,6 @@ int main() {
               << std::endl;
     std::cout << "Base orientation after predict " << predicted_state.base_orientation_<<std::endl;
     std::cout << "Left contact position after predict "
-              << predicted_state.foot_pose_.at("left_foot").translation().transpose() << std::endl;
+              << predicted_state.contacts_position_.at("left_foot").transpose() << std::endl;
     return 0;
 }
