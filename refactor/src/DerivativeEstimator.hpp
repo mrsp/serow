1a213ade0c4fe3f120a69944eaa4bcbafdd2d410
mrsp <stpiperakis@gmail.com>
2024-02-28 11:32:42 +0200

    license update

--- refactor/src/DerivativeEstimator.hpp
+++ refactor/src/DerivativeEstimator.hpp
@@ -1,15 +1,23 @@
-/*
- * Copyright Stylianos Piperakis, Ownage Dynamics L.P.
- * License: GNU: https://www.gnu.org/licenses/gpl-3.0.html
- */
+/**
+* Copyright (C) 2024 Stylianos Piperakis, Ownage Dynamics L.P.
+* Serow is free software: you can redistribute it and/or modify it under the terms of the GNU 
+* General Public License as published by the Free Software Foundation, version 3.
+* 
+* Serow is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+* even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+* General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License along with Serow. If not, 
+* see <https://www.gnu.org/licenses/>.
+**/
 /**
  * @brief Signal derivative estimation with numerical differentiation and
  * 2nd order Low Pass Butterworth Filter
  * @author Stylianos Piperakis
  * @details estimates the measurement's 3D derivative
  */
-
 #pragma once
+
 #include "ButterworthLPF.hpp"
 #include "Differentiator.hpp"
 
--- refactor/src/Differentiator.hpp
+++ refactor/src/Differentiator.hpp
@@ -1,14 +1,22 @@
-/*
- * Copyright Stylianos Piperakis, Ownage Dynamics L.P.
- * License: GNU: https://www.gnu.org/licenses/gpl-3.0.html
- */
 /**
- * @brief Numerical Differentiation with the First Order Euler Method
+* Copyright (C) 2024 Stylianos Piperakis, Ownage Dynamics L.P.
+* Serow is free software: you can redistribute it and/or modify it under the terms of the GNU 
+* General Public License as published by the Free Software Foundation, version 3.
+* 
+* Serow is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+* even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+* General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License along with Serow. If not, 
+* see <https://www.gnu.org/licenses/>.
+**/
+/**
+ * @brief Numerical differentiation with the first order Euler method
  * @author Stylianos Piperakis
  * @details to numerical compute the derivative of a signal
  */
-
 #pragma once
+
 #include <string>
 
 namespace serow {
