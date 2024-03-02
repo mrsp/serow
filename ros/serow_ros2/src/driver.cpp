c43ad9170e3937d1faa3319bb28ce190730e8f27
mrsp <stpiperakis@gmail.com>
2024-03-02 12:11:58 +0200

    wip ros2 driver

---
+++ ros/serow_ros2/src/driver.cpp
@@ -0,0 +1,43 @@
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/wrench.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+#include "sensor_msgs/msg/imu.hpp"
+
+using std::placeholders::_1;
+
+class SerowDriver : public rclcpp::Node {
+   public:
+    SerowDriver(const std::string& joint_state_topic, const std::string& imu_topic,
+                const std::vector<std::string>& force_torque_state_topics)
+        : Node("serow_driver") {
+        joint_state_subscription_ = this->create_subscription<sensor_msgs::msg::JointState>(
+            joint_state_topic, 1000, std::bind(&SerowDriver::joint_state_topic_callback, this, _1));
+        imu_subscription_ = this->create_subscription<sensor_msgs::msg::Imu>(
+            imu_topic, 1000, std::bind(&SerowDriver::imu_topic_callback, this, _1));
+    }
+
+   private:
+    void joint_state_topic_callback(const sensor_msgs::msg::JointState& msg) const {
+        RCLCPP_INFO(this->get_logger(), "joint state callback");
+    }
+    
+    void imu_topic_callback(const sensor_msgs::msg::Imu& msg) const {
+        RCLCPP_INFO(this->get_logger(), "imu callback");
+    }
+
+    rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_state_subscription_;
+    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_subscription_;
+
+}; 
+
+
+int main(int argc, char* argv[]) {
+    rclcpp::init(argc, argv);
+    std::vector<std::string> force_torque_state_topics;
+    force_torque_state_topics.push_back("/left_leg/force_torque_states");
+    force_torque_state_topics.push_back("/right_leg/force_torque_states");
+
+    rclcpp::spin(std::make_shared<SerowDriver>("/joint_states", "/imu0", force_torque_state_topics));
+    rclcpp::shutdown();
+    return 0;
+}
