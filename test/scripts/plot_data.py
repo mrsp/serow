5c1b58818fd2e4d000107c69cf73a55e85860ee0
MichaelMarav <michaelmarav4.gmail.com>
2024-04-16 17:00:03 +0300

    GO1 serow estimates fixed. Do not produce nan

--- test/scripts/plot_data.py
+++ test/scripts/plot_data.py
@@ -1,7 +1,3 @@
-'''
-Plots the data stored in the file specified at plot_config.
-Prints the bias in the forces for each leg
-'''
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -10,20 +6,32 @@ import yaml
 config_filename = "plot_config.yaml"
 
 with open(config_filename, "r") as file:
-  config_options = yaml.load(file, Loader=yaml.FullLoader)
+    config_options = yaml.load(file, Loader=yaml.FullLoader)
 
 csv_filename = config_options["data_filename"]
-
+force_column_indxs = config_options["force_column_indxs"]
 df = pd.read_csv(csv_filename)
 
-# Extract y-labels from column names
-y_labels = [label.split(",")[0] for label in df.columns[1:]]
+# Compute the low bias for each force column
+low_bias_values = df.iloc[:, force_column_indxs].min()
+
+# Subtract the low bias from all measurements in each force column
+df.iloc[:, force_column_indxs] -= low_bias_values.values
+
+# Get column names using .iloc
+column_names = df.columns.tolist()
+
+# Print the biases for each force column
+print("Bias values:")
+for idx, bias in zip(force_column_indxs, low_bias_values):
+    print(f"Force {column_names[idx]} --> {bias}")
 
 # Plot data
-for i, column in enumerate(df.columns[1:]):
+for i, idx in enumerate(force_column_indxs):
     plt.figure(figsize=(10, 6))
-    plt.plot(df.iloc[:, 0].values, df[column].values)  # Convert DataFrame columns to numpy arrays
-    plt.xlabel(df.columns[0])  # Set xlabel dynamically from the first column
-    plt.ylabel(column)
-    plt.title(f"{column} vs. {df.columns[0]}")
+    plt.plot(df.iloc[:, 0].values, df.iloc[:, idx].values)  # Use column index from config
+    plt.xlabel(df.columns.values[0])  # Set xlabel dynamically from the first column
+    plt.ylabel(df.columns.values[idx])  # Set ylabel dynamically
+    plt.title(f"{df.columns[idx]} vs. {df.columns[0]}")
+    plt.ylim(0, df.iloc[:, idx].max())  # Set y-axis limit from 0 to max value
     plt.show()
