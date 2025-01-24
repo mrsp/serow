66740bb81ed378c61ae0b4cfcf018f18a89b633a
Stylianos Piperakis <stpiperakis@gmail.com>
2025-01-24 12:22:43 +0200

    Feature/BaseEKF (#24)
    * initial pass
    
    * wip
    
    * remove results

--- core/src/ContactEKF.cpp
+++ core/src/ContactEKF.cpp
@@ -16,51 +16,6 @@
 
 namespace serow {
 
-double OutlierDetector::computePsi(double xxx) {
-    double result = 0;
-    double xx, xx2, xx4;
-    for (; xxx < 7; ++xxx) {
-        result -= 1 / xxx;
-    }
-
-    xxx -= 1.0 / 2.0;
-    xx = 1.0 / xxx;
-    xx2 = xx * xx;
-    xx4 = xx2 * xx2;
-    result += std::log(xxx) + (1.0 / 24.0) * xx2 - (7.0 / 960.0) * xx4 +
-              (31.0 / 8064.0) * xx4 * xx2 - (127.0 / 30720.0) * xx4 * xx4;
-    return result;
-}
-
-void OutlierDetector::init() {
-    zeta = 1.0;
-    e_t = e_0;
-    f_t = f_0;
-}
-
-void OutlierDetector::estimate(const Eigen::Matrix3d& BetaT, const Eigen::Matrix3d& R) {
-    double efpsi = computePsi(e_t + f_t);
-    double lnp = computePsi(e_t) - efpsi;
-    double ln1_p = computePsi(f_t) - efpsi;
-
-    double pzeta_1 = std::exp(lnp - 0.5 * (BetaT * R.inverse()).trace());
-    double pzeta_0 = std::exp(ln1_p);
-
-    // Normalization factor
-    double norm_factor = 1.0 / (pzeta_1 + pzeta_0);
-
-    // p(zeta) are now proper probabilities
-    pzeta_1 = norm_factor * pzeta_1;
-    pzeta_0 = norm_factor * pzeta_0;
-
-    // Mean of Bernulli
-    zeta = pzeta_1 / (pzeta_1 + pzeta_0);
-
-    // Update epsilon and f
-    e_t = e_0 + zeta;
-    f_t = f_0 + 1.0 - zeta;
-}
-
 void ContactEKF::init(const BaseState& state, std::set<std::string> contacts_frame, bool point_feet,
                       double g, double imu_rate, bool outlier_detection) {
     num_leg_end_effectors_ = contacts_frame.size();
@@ -414,9 +369,9 @@ BaseState ContactEKF::updateWithOdometry(const BaseState& state,
     R.topLeftCorner<3, 3>() = base_position_cov;
     R.bottomRightCorner<3, 3>() = base_orientation_cov;
 
-    const Eigen::Matrix3d& s = R + H * P_ * H.transpose();
-    const Eigen::MatrixXd& K = P_ * H.transpose() * s.inverse();
-    const Eigen::VectorXd& dx = K * z;
+    const Eigen::Matrix<double, 6, 6> s = R + H * P_ * H.transpose();
+    const Eigen::MatrixXd K = P_ * H.transpose() * s.inverse();
+    const Eigen::VectorXd dx = K * z;
 
     P_ = (I_ - K * H) * P_;
     updateState(updated_state, dx, P_);
