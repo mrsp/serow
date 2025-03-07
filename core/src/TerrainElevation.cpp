7bc9bf727f5bf3383c05d65b68ad6e9fbaa56da0
mrsp <stpiperakis@gmail.com>
2025-03-07 11:46:51 +0200

    compiles

---
+++ core/src/TerrainElevation.cpp
@@ -0,0 +1,196 @@
+#include "TerrainElevation.hpp"
+
+namespace serow {
+
+int TerrainElevation::locationToGlobalIndex(const float loc) const {
+    return static_cast<int>(resolution_inv * loc + loc > 0.0 ? 0.5 : -0.5);
+}
+
+std::array<int, 2> TerrainElevation::locationToGlobalIndex(const std::array<float, 2>& loc) const {
+    return {static_cast<int>(resolution_inv * loc[0] + loc[0] > 0.0 ? 0.5 : -0.5),
+            static_cast<int>(resolution_inv * loc[1] + loc[1] > 0.0 ? 0.5 : -0.5)};
+}
+
+bool TerrainElevation::inside(const std::array<int, 2>& id_g) const {
+    if ((abs(id_g[0] - local_map_origin_i_[0]) - half_map_dim[0]) > 0 ||
+        (abs(id_g[1] - local_map_origin_i_[1]) - half_map_dim[1]) > 0) {
+        return false;
+    }
+    return true;
+}
+
+bool TerrainElevation::inside(const std::array<float, 2>& location) const {
+    return inside(locationToGlobalIndex(location));
+}
+
+void TerrainElevation::resetCell(const int& hash_id) { height_[hash_id] = 0; }
+
+void TerrainElevation::resetLocalMap() { std::fill(height_.begin(), height_.end(), 0.0); }
+
+void TerrainElevation::initializeLocalMap(const float height) {
+    std::fill(height_.begin(), height_.end(), height);
+}
+
+void TerrainElevation::updateLocalMapOriginAndBound(const std::array<float, 2>& new_origin_d,
+                                                    const std::array<int, 2>& new_origin_i) {
+    // update local map origin and local map bound
+    local_map_origin_i_ = new_origin_i;
+    local_map_origin_d_ = new_origin_d;
+
+    local_map_bound_max_i_ = {local_map_origin_i_[0] + half_map_dim[0],
+                              local_map_origin_i_[1] + half_map_dim[1]};
+    local_map_bound_min_i_ = {local_map_origin_i_[0] - half_map_dim[0],
+                              local_map_origin_i_[1] - half_map_dim[1]};
+
+    // the float map bound only consider the closed cell center
+    local_map_bound_min_d_ = globalIndexToLocation(local_map_bound_min_i_);
+    local_map_bound_max_d_ = globalIndexToLocation(local_map_bound_max_i_);
+}
+
+void TerrainElevation::clearOutOfMapCells(const std::vector<int>& clear_id, const int& i) {
+    std::vector<int> ids{i, (i + 1) % 2};
+    for (const auto& x : clear_id) {
+        for (int y = -half_map_dim[ids[1]]; y <= half_map_dim[ids[1]]; y++) {
+            const std::array<int, 2> temp_clear_id = {x, y};
+            resetCell(localIndexToHashId(temp_clear_id));
+        }
+    }
+}
+
+void TerrainElevation::recenter(const std::array<float, 2>& location) {
+    // Compute the shifting index
+    const std::array<int, 2> new_origin_i = locationToGlobalIndex(location);
+    const std::array<float, 2> new_origin_d = {new_origin_i[0] * resolution,
+                                               new_origin_i[1] * resolution};
+
+    // Compute the delta shift
+    const std::array<int, 2> shift_num = {new_origin_i[0] - local_map_origin_i_[0],
+                                          new_origin_i[1] - local_map_origin_i_[1]};
+    for (size_t i = 0; i < 2; i++) {
+        if (fabs(shift_num[i]) > map_dim[i]) {
+            // Clear all map
+            resetLocalMap();
+            updateLocalMapOriginAndBound(new_origin_d, new_origin_i);
+            return;
+        }
+    }
+
+    // Clear the memory out of the map size
+    for (size_t i = 0; i < 2; i++) {
+        if (shift_num[i] == 0) {
+            continue;
+        }
+        const int min_id_g = -half_map_dim[i] + local_map_origin_i_[i];
+        const int min_id_l = min_id_g % map_dim[i];
+        std::vector<int> clear_id;
+        if (shift_num[i] > 0) {
+            // forward shift, the min id should be cut
+            for (int k = 0; k < shift_num[i]; k++) {
+                int temp_id = min_id_l + k;
+                temp_id = normalize(temp_id, -half_map_dim[i], half_map_dim[i]);
+                clear_id.push_back(temp_id);
+            }
+        } else {
+            // backward shift, the max should be shifted
+            for (int k = -1; k >= shift_num[i]; k--) {
+                int temp_id = min_id_l + k;
+                temp_id = normalize(temp_id, -half_map_dim[i], half_map_dim[i]);
+                clear_id.push_back(temp_id);
+            }
+        }
+
+        if (clear_id.empty()) {
+            continue;
+        }
+        clearOutOfMapCells(clear_id, i);
+    }
+
+    updateLocalMapOriginAndBound(new_origin_d, new_origin_i);
+}
+
+std::array<float, 2> TerrainElevation::globalIndexToLocation(const std::array<int, 2>& id_g) const {
+    return {id_g[0] * resolution, id_g[1] * resolution};
+}
+
+std::array<int, 2> TerrainElevation::globalIndexToLocalIndex(const std::array<int, 2>& id_g) const {
+    std::array<int, 2> id_l = {};
+
+    for (size_t i = 0; i < 2; i++) {
+        id_l[i] = id_g[i] % map_dim[i];
+        if (id_l[i] > half_map_dim[i]) {
+            id_l[i] -= map_dim[i];
+        } else if (id_l[i] < -half_map_dim[i]) {
+            id_l[i] += map_dim[i];
+        }
+    }
+    return id_l;
+}
+
+std::array<int, 2> TerrainElevation::localIndexToGlobalIndex(const std::array<int, 2>& id_l) const {
+    std::array<int, 2> id_g = {};
+    for (size_t i = 0; i < 2; i++) {
+        int min_id_g = -half_map_dim[i] + local_map_origin_i_[i];
+        int min_id_l = min_id_g % map_dim[i];
+        min_id_l -= min_id_l > half_map_dim[i] ? map_dim[i] : 0;
+        min_id_l += min_id_l < -half_map_dim[i] ? map_dim[i] : 0;
+
+        int cur_dis_to_min_id = id_l[i] - min_id_l;
+        cur_dis_to_min_id =
+            cur_dis_to_min_id < 0 ? map_dim[i] + cur_dis_to_min_id : cur_dis_to_min_id;
+        int cur_id = cur_dis_to_min_id + min_id_g;
+        id_g[i] = cur_id;
+    }
+    return id_g;
+}
+
+std::array<float, 2> TerrainElevation::localIndexToLocation(const std::array<int, 2>& id_l) const {
+    std::array<float, 2> loc = {};
+    for (size_t i = 0; i < 2; i++) {
+        int min_id_g = -half_map_dim[i] + local_map_origin_i_[i];
+        int min_id_l = min_id_g % map_dim[i];
+        min_id_l -= min_id_l > half_map_dim[i] ? map_dim[i] : 0;
+        min_id_l += min_id_l < -half_map_dim[i] ? map_dim[i] : 0;
+
+        int cur_dis_to_min_id = id_l[i] - min_id_l;
+        cur_dis_to_min_id =
+            cur_dis_to_min_id < 0 ? map_dim[i] + cur_dis_to_min_id : cur_dis_to_min_id;
+        int cur_id = cur_dis_to_min_id + min_id_g;
+        loc[i] = cur_id * resolution;
+    }
+    return loc;
+}
+
+std::array<int, 2> TerrainElevation::hashIdToLocalIndex(const int hash_id) const {
+    int id0 = hash_id / map_dim[1];
+    int id1 = hash_id - id0 * map_dim[1];
+    return {id0 - half_map_dim[0], id1 - half_map_dim[1]};
+}
+
+std::array<int, 2> TerrainElevation::hashIdToGlobalIndex(const int hash_id) const {
+    return localIndexToGlobalIndex(hashIdToLocalIndex(hash_id));
+}
+
+std::array<float, 2> TerrainElevation::hashIdToLocation(const int hash_id) const {
+    return localIndexToLocation(hashIdToLocalIndex(hash_id));
+}
+
+int TerrainElevation::localIndexToHashId(const std::array<int, 2>& id_in) const {
+    const std::array<int, 2> id = {id_in[0] + half_map_dim[0], id_in[1] + half_map_dim[1]};
+    return id[0] * map_dim[1] + id[1];
+}
+
+int TerrainElevation::locationToHashId(const std::array<float, 2>& loc) const {
+    return localIndexToHashId(globalIndexToLocalIndex(locationToGlobalIndex(loc)));
+}
+
+int TerrainElevation::globalIndexToHashId(const std::array<int, 2>& id_g) const {
+    return localIndexToHashId(globalIndexToLocalIndex(id_g));
+}
+
+int TerrainElevation::normalize(int x, int a, int b) {
+    const int range = b - a + 1;
+    const int y = (x - a) % range;
+    return (y < 0 ? y + range : y) + a;
+}
+
+}  // namespace serow
--- core/src/TerrainElevation.hpp
+++ core/src/TerrainElevation.hpp
@@ -1,140 +1,79 @@
 #pragma once
 
-#include <iostream>
+#include <array>
 #include <cmath>
+#include <iostream>
+#include <vector>
 
 namespace {
-static constexpr uint16_t size_x = 1000;
-static constexpr uint16_t size_y = 1000;
-static constexpr float resolution = 0.05; 
-static constexpr float resolution_inv = 1.0 / resolution; 
-static constexpr uint16_t map_size = size_x * size_y;
-static constexpr uint16_t half_map_size = map_size / 2;
-static constexpr std::array<int16_t, 2> map_dim = {size_x, size_y};
-static constexpr std::array<int16_t, 2> half_map_dim = {size_x / 2, size_y / 2};
-}
+
+static constexpr int size_x = 1000;
+static constexpr int size_y = 1000;
+static constexpr float resolution = 0.05;
+static constexpr float resolution_inv = 1.0 / resolution;
+static constexpr int map_size = size_x * size_y;
+static constexpr int half_map_size = map_size / 2;
+static constexpr std::array<int, 2> map_dim = {size_x, size_y};
+static constexpr std::array<int, 2> half_map_dim = {size_x / 2, size_y / 2};
+
+}  // namespace
 
 namespace serow {
 
-
 class TerrainElevation {
-public:
+   public:
+    bool inside(const std::array<int, 2>& id_g) const;
 
-    bool inside(const std::array<int16_t, 2>& id_g) {
-        if ((abs(id_g[0] - local_map_origin_i[0]) - half_map_dim[0]) > 0 || 
-             (abs(id_g[1] - local_map_origin_i[1]) - half_map_dim[1]) > 0) {
-            return false;
-        }
-        return true;
-    }
+    bool inside(const std::array<float, 2>& location) const;
 
-    bool inside(const std::array<double, 2>& location) {
-        return inside(locationToGlobalIndex(location));
-    }
+    void resetCell(const int& hash_id);
 
-    void clearOutOfMapCells(const std::vector<int>& clear_id, const int& i) {
-        std::vector<int> ids{i, (i + 1) % 2};
-        for (const auto& x: clear_id) {
-            for (int y = -sc_.half_map_size_i(ids[1]); y <= sc_.half_map_size_i(ids[1]); y++) {
-                    const std::array<int16_t, 2> temp_clear_id = {x, y};
-                    resetCell(getLocalIndexHash(temp_clear_id));
-                } 
-            }
-        }
-    }
+    void updateLocalMapOriginAndBound(const std::array<float, 2>& new_origin_d,
+                                      const std::array<int, 2>& new_origin_i);
 
-    void recenter() {
+    void clearOutOfMapCells(const std::vector<int>& clear_id, const int& i);
 
-    }
+    void recenter(const std::array<float, 2>& location);
 
-    int16_t locationToGlobalIndex(const float loc) {
-        return static_cast<int16_t>(resolution_inv * loc + loc > 0.0 ? 0.5 : -0.5);
-    } 
+    int locationToGlobalIndex(const float loc) const;
 
-    std::array<int16_t, 2> locationToGlobalIndex(const std::array<float, 2>& loc) {
-        return {resolution_inv * loc[0] + loc[0] > 0.0 ? 0.5 : -0.5, 
-               {resolution_inv * loc[1] + loc[1] > 0.0 ? 0.5 : -0.5};
-    } 
+    std::array<int, 2> locationToGlobalIndex(const std::array<float, 2>& loc) const;
 
-    std::array<float, 2> globalIndexToLocation(const std::array<int16_t, 2>& id_g) const {
-        return {id_g[0] * resolution, id_g[1] * resolution};
-    }
+    std::array<float, 2> globalIndexToLocation(const std::array<int, 2>& id_g) const;
 
-    std::array<int16_t, 2> globalIndexToLocalIndex(const std::array<int16_t, 2>& id_g) const {
-        std::array<int16_t, 2> id_l = {};
+    std::array<int, 2> globalIndexToLocalIndex(const std::array<int, 2>& id_g) const;
 
-        for (size_t i = 0; i < 2; i++) {
-            id_l[i] = id_g[i] % map_dim[i];
-            if (id_l[i] > half_map_dim[i]) {
-                id_l[i] -= map_dim[i];
-            } else if (id_l[i] < -half_map_dim[i]) {
-                id_l[i] += map_dim[i];
-            } 
-        }
-        return id_l;
-    }
+    std::array<int, 2> localIndexToGlobalIndex(const std::array<int, 2>& id_l) const;
 
-    std::array<int16_t, 2> localIndexToGlobalIndex(const std::array<int16_t, 2>& id_l) const {
-        std::array<int16_t, 2> id_g = {};
-        for (size_t i = 0; i < 2; i++) {
-            int16_t min_id_g = -half_map_dim[i] + local_map_origin_i_[i];
-            int16_t min_id_l = min_id_g % map_dim[i];
-            min_id_l -= min_id_l > half_map_dim[i] ? map_dim[i] : 0;
-            min_id_l += min_id_l < -half_map_dim[i] ? map_dim[i] : 0;
+    std::array<float, 2> localIndexToLocation(const std::array<int, 2>& id_l) const;
 
-            int16_t cur_dis_to_min_id = id_l[i] - min_id_l;
-            cur_dis_to_min_id = cur_dis_to_min_id < 0 ? map_dim[i] + cur_dis_to_min_id : cur_dis_to_min_id;
-            int16_t cur_id = cur_dis_to_min_id + min_id_g;
-            id_g[i] = cur_id;
-        }
-        return id_g;
-    }
-    
-    std::array<float, 2> localIndexToLocation(const std::array<int16_t, 2>& id_l) const {
-        std::array<float, 2> loc = {};
-        for (size_t i = 0; i < 2; i++) {
-            int16_t min_id_g = -half_map_dim[i] + local_map_origin_i_[i];
-            int16_t min_id_l = min_id_g % map_dim[i];
-            min_id_l -= min_id_l > half_map_dim[i] ? map_dim[i] : 0;
-            min_id_l += min_id_l < -half_map_dim[i] ? map_dim[i] : 0;
+    std::array<int, 2> hashIdToLocalIndex(const int hash_id) const;
 
-            int16_t cur_dis_to_min_id = id_l[i] - min_id_l;
-            cur_dis_to_min_id = cur_dis_to_min_id < 0 ? map_dim[i] + cur_dis_to_min_id : cur_dis_to_min_id;
-            int16_t cur_id = cur_dis_to_min_id + min_id_g;
-            loc[i] = cur_id * resolution;
-        }
-        return loc;
-    }
+    std::array<int, 2> hashIdToGlobalIndex(const int hash_id) const;
 
-    std::array<int16_t, 2> hashIdToLocalIndex(const int hash_id) const {
-        int16_t id0 = hash_id / map_dim[1];
-        int16_t id1 = hash_id - id0 * map_dim[1];
-        return {id0 - half_map_dim[0], id1 - half_map_dim[1]};
-    }
+    std::array<float, 2> hashIdToLocation(const int hash_id) const;
 
-    std::array<int16_t, 2> hashIdToGlobalIndex(const int hash_id) const {
-        return localIndexToGlobalIndex(hashIdToLocalIndex(hash_id));
-    }
+    int localIndexToHashId(const std::array<int, 2>& id_in) const;
 
-    std::array<float, 2> hashIdToLocation(const int hash_id) const {
-        return localIndexToLocation(hashIdToLocalIndex(hash_id));
-    }
+    int locationToHashId(const std::array<float, 2>& loc) const;
 
-    int localIndexToHashId(const std::array<int16_t, 2>& id_in) const {
-        const std::array<int16_t, 2> id = {id_in[0] + half_map_dim[0], id_in[1] + half_map_dim[1]};
-        return id[0] * map_dim[1] + id[1];
-    }
+    int globalIndexToHashId(const std::array<int, 2>& id_g) const;
 
-    int locationToHashId(const std::array<float, 2>& loc) const {
-        return localIndexToHashId(globalIndexToLocalIndex(locationToGlobalIndex(loc)));
-    }
+    void initializeLocalMap(const float height);
 
+    void resetLocalMap();
 
-    int globalIndexToHashId(const std::array<int16_t, 2>& id_g) const {
-        return localIndexToHashId(globalIndexToLocalIndex(id_g));
-    }
-private:
+   private:
     std::array<float, map_size> height_{};
+
+    std::array<int, 2> local_map_origin_i_{};
+    std::array<int, 2> local_map_bound_max_i_{};
+    std::array<int, 2> local_map_bound_min_i_{};
+    std::array<float, 2> local_map_origin_d_{};
+    std::array<float, 2> local_map_bound_max_d_{};
+    std::array<float, 2> local_map_bound_min_d_{};
+
+    int normalize(int x, int a, int b);
 };
 
-} 
+}  // namespace serow
