819d203d0bd2acb82fdcbb840fea6861247cdeb8
Stylianos Piperakis <stpiperakis@gmail.com>
2025-03-24 20:56:46 +0200

    nitpick

--- ros2/serow_ros2/launch/h1.py
+++ ros2/serow_ros2/launch/h1.py
@@ -1,9 +1,11 @@
+import os
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 from ament_index_python.packages import get_package_share_directory
-import os
+
 
 def generate_launch_description():
     # Get the path to the config file
