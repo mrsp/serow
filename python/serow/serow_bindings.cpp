caddd7eaf4c4f35087685686f9ee9b7f3ae6c69d
mrsp <stpiperakis@gmail.com>
2025-05-19 16:45:19 +0300

    fix bindings

--- python/serow/serow_bindings.cpp
+++ python/serow/serow_bindings.cpp
@@ -237,6 +237,8 @@ PYBIND11_MODULE(serow, m) {
         .def("init", &serow::ContactEKF::init, 
              py::arg("state"), py::arg("contacts_frame"), py::arg("point_feet"), 
              py::arg("g"), py::arg("imu_rate"), py::arg("outlier_detection") = false,
+             py::arg("use_onnx") = false, py::arg("robot_name") = "",
+             py::arg("model_path") = "policy/ddpg",
              "Initializes the EKF with the initial robot state and parameters")
         .def("predict", &serow::ContactEKF::predict, 
              py::arg("state"), py::arg("imu"), py::arg("kin"),
