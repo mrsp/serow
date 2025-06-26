6fdc3baf813c61a2b83ea8a51a357680ff3441e7
Michael Maravgakis <michaelmarav4@gmail.com>
2025-06-26 17:16:20 +0300

    Added check to make sure hash_id is inside elevation (#50)
    * Added check to make sure hash_id is inside elevation (fixes the hang due to corrupt memory probably)
    
    * Update core/src/LocalTerrainMapper.cpp
    
    * Added check to setElevation too
    
    ---------
    
    Co-authored-by: MichaelMarav <michaelmarav4.gmail.com>
    Co-authored-by: Stylianos Piperakis <stpiperakis@gmail.com>

--- core/src/LocalTerrainMapper.cpp
+++ core/src/LocalTerrainMapper.cpp
@@ -232,11 +232,19 @@ bool LocalTerrainMapper::update(const std::array<float, 2>& loc, float height, f
 }
 
 std::optional<ElevationCell> LocalTerrainMapper::getElevation(const std::array<float, 2>& loc) {
+    std::lock_guard lock(mutex_);
+
     if (!inside(loc)) {
         return std::nullopt;
     }
+    
     const int hash_id = locationToHashId(loc);
-    std::lock_guard<std::mutex> lock(mutex_);
+    
+    // Add bounds check!
+    if (hash_id < 0 || hash_id >= static_cast<int>(elevation_.size())) {
+        return std::nullopt;
+    }
+    
     return elevation_[hash_id];
 }
 
@@ -272,11 +280,18 @@ bool LocalTerrainMapper::isHashIdValid(const int id) const {
 
 bool LocalTerrainMapper::setElevation(const std::array<float, 2>& loc,
                                       const ElevationCell& elevation) {
+
+    std::lock_guard<std::mutex> lock(mutex_);
     if (!inside(loc)) {
         return false;
     }
     const int idx = locationToHashId(loc);
-    std::lock_guard<std::mutex> lock(mutex_);
+        
+    // Add bounds check!
+    if (idx < 0 || idx >= static_cast<int>(elevation_.size())) {
+        return false;
+    }
+    
     elevation_[idx] = elevation;
     return true;
 }
